63120827d780be3e5c5339039a46946fc9353576
Tysmithyyy <tylerelijahsmith15@gmail.com>
2023-09-26 15:14:29 -0600

    added email field and email validation

--- sms_form.py
+++ sms_form.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import re
 
-st.title('Sign up for my texting list!')
+st.title('Sign up to be on the texting list for Melinda Smith Photography!')
 
 credentials = {
     "type": st.secrets.sheets_credentials.type,
@@ -22,6 +23,18 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
 client = gspread.authorize(creds)
 
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+def email_check(email):
+ 
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if(re.fullmatch(regex, email)):
+        return True
+ 
+    else:
+        return False
+
 success_message = ""
 error_message = ""
 
@@ -38,28 +51,35 @@ with container.container():
 
     phone_value = st.text_input("Phone Number", max_chars=10, help=help_msg)
 
-    
+    email_value = st.text_input("Email (optional)")
 
-    contact = [first_name_value, last_name_value, phone_value]
+    contact = [first_name_value, last_name_value, phone_value, email_value]
 
     if st.button("Submit"):
         if first_name_value and phone_value and last_name_value and len(phone_value)>9:
-            # Open the Google Sheet by title
-            sheet = client.open("contacts sheet")
+            if email_value:
+                if email_check(email_value) == False:
+                    error_message = f"Please enter a valid email to submit."
+                    email_value = ""
+            if not error_message:
+                # Open the Google Sheet by title
+                sheet = client.open("contacts sheet")
+                
+                # Select the worksheet where you want to add the row
+                worksheet = sheet.get_worksheet(0)  # Use the index (0-based) or title of the worksheet
 
-            # Select the worksheet where you want to add the row
-            worksheet = sheet.get_worksheet(0)  # Use the index (0-based) or title of the worksheet
+                # Append the row to the worksheet
+                worksheet.append_row(contact)
+                
+                container.empty()
 
-            # Append the row to the worksheet
-            worksheet.append_row(contact)
-            
-            container.empty()
-
-            success_message = "Thanks for signing up! Your number has been saved."
+                success_message = "Thanks for signing up! Your info has been saved."
         else:
             if phone_value.isnumeric() == False or len(phone_value)<10:
                 missing_value = "10 digit phone number"
                 phone_value = ""
+            elif email_check(email_value) == False:
+                missing_value = "valid email"
             elif not first_name_value:
                 missing_value = "first name"
             elif not last_name_value:
